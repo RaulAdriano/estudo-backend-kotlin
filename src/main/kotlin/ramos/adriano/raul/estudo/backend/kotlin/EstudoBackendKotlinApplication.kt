c882ad34a3971f50982f9a1c2ef65ec34036e042
Raul Adriano Ramos <raul.adriano.ramos@gmail.com>
2022-08-25 23:45:03 -0300

    projeto inicial

---
+++ src/main/kotlin/ramos/adriano/raul/estudo/backend/kotlin/EstudoBackendKotlinApplication.kt
@@ -0,0 +1,11 @@
+package ramos.adriano.raul.estudo.backend.kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EstudoBackendKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<EstudoBackendKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/ramos/adriano/raul/estudo/backend/kotlin/EstudoBackendKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package ramos.adriano.raul.estudo.backend.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EstudoBackendKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
